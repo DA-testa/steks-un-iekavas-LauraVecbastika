f6645a4a05f1fb082e285ceedf696a38c1b3c2ab
LauraVecbastika <124885748+LauraVecbastika@users.noreply.github.com>
2023-02-15 15:15:21 +0000

    Test

--- main.py
+++ main.py
@@ -15,16 +15,15 @@ def find_mismatch(text):
         if next in "([{":
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i+1))
-            pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if(opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next)):
+            if(not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next)):
                 return i+1
             opening_brackets_stack.pop()
-            if opening_brackets_stack:
-                return opening_brackets_stack[0].position
-        return "Success"
+        # if opening_brackets_stack:
+        #     return opening_brackets_stack[-1].position
+    return "Success"
 
 
 def main():
